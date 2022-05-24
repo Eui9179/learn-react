5b054ae128f2231917c2d63af0c1e7f5edea1a52
eui9179 <outdark97@gmail.com>
2022-05-25 02:53:04 +0900

    props

--- helloworld/src/PropsStudy/index.jsx
+++ helloworld/src/PropsStudy/index.jsx
@@ -4,10 +4,7 @@ import { Name } from './style';
 const index = ({ name = 'eui', isSpecial = false }) => {
   return (
     <Name>
-      <div>
-        {isSpecial ? <b>*</b> : null}
-        {name}
-      </div>
+      <div>{name}</div>
     </Name>
   );
 };
