1dec5fe0d23ca31ab86d156fa17569d4161f914c
eui9179 <outdark97@gmail.com>
2022-05-25 21:35:01 +0900

    todo-list-practice

---
+++ todolist-practice/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // submit 후 새로고침 방지
+    if (toDo === '') {
+      // toDo가 비어있는 상태로 호출하면 아무 동작도 안함
+      return;
+    }
+
+    setToDos((prev) => [toDo, ...prev]);
+
+    setToDo(''); // toDo state 비워주기
+  };
+
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
